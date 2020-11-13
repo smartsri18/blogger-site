d155cad9272868e35d25b34221f5e856ce20f5e5
smartsri18 <sridharan479@gmail.com>
2020-11-13 22:19:53 +0530

    controllers added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_080846) do
+ActiveRecord::Schema.define(version: 2020_11_12_103152) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -32,7 +32,12 @@ ActiveRecord::Schema.define(version: 2020_11_12_080846) do
     t.integer "user_type", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
