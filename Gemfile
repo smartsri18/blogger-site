304b3b6fe21e41f9f1c61f68b6035af29a0c405a
smartsri18 <sridharan479@gmail.com>
2020-11-15 19:14:41 +0530

    simple form and devise views added

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'devise'
 gem 'bootstrap'
 gem 'jquery-rails'
 gem "font-awesome-rails"
+gem 'simple_form'
+gem 'bootstrap-sass-extras'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :articles, dependent: :destroy
 
-  has_secure_password
+  # has_secure_password
 
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
