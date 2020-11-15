c1a8961c78112d9d076745ac2037a79276b81c16
smartsri18 <sridharan479@gmail.com>
2020-11-15 15:56:42 +0530

    jquery and other gems added

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'rails_admin'
 gem 'devise'
+gem 'bootstrap'
+gem 'jquery-rails'
+gem "font-awesome-rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'pages#index'
+  get '/about_us', to: 'pages#about'
 end
